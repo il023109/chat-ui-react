eb020f77799e9a9ad2364c20ac58c55d58370566
il023109 <il023109@users.noreply.github.com>
2023-02-07 23:19:02 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,21 @@ export { MuiSelectInput } from './mui/MuiSelectInput';
 export { MuiMultiSelectInput } from './mui/MuiMultiSelectInput';
 export { MuiFileInput } from './mui/MuiFileInput';
 export { MuiAudioInput } from './mui/MuiAudioInput';
+
+
+function App(): React.ReactElement {
+  const [chatCtl] = React.useState(new ChatController());
+
+  React.useMemo(async () => {
+    // Chat content is displayed using ChatController
+    await chatCtl.addMessage({
+      type: 'text',
+      content: `Hello, What's your name.`,
+      self: false,
+    });
+    const name = await chatCtl.setActionRequest({ type: 'text' });
+  }, [chatCtl]);
+
+  // Only one component used for display
+  return <MuiChat chatController={chatCtl} />;
+}
